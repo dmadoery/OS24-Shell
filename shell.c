03003ae75d0f9d1b0ca001a7b68bdfde337a5911
ChHungerbuehler <christian.hungerbuehler@stud.unibas.ch>
2024-04-24 13:54:25 +0200

    adding function split()

--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-#include<stdio.h> 
+#include<stdio.h>
 #include<string.h> 
 #include<stdlib.h> 
 #include<unistd.h> 
@@ -11,12 +11,29 @@
 
 //parser for "|"
 void pipe_detector() {
-    //TODO Implement
+    //TODO Implement: perhabs could be combined into a single split() function with parameter delim set to "|" or " "
 }
 
 //parser for " "
 void space_parser() {
-    //TODO Implement
+    //TODO Implement: perhabs could be combined into a single split() function with parameter delim set to "|" or " "
+}
+
+/* combines pipe_detector() and space_parser() */
+char ** split(char *in_str, char* delim) {
+		// count how often the delimiter delim appears in the string in_str
+		char *tmp = in_str;
+		int counter = 0;
+		char **out_str;
+		
+		while (*tmp) {
+				if (*tmp == *delim) {
+						counter++;
+				}
+				tmp++;
+		}
+		// https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
+		
 }
 
 //executes piped and single commands via child processes with pipe communication if there are multiple cmds
