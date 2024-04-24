6c6343185926b9c88426d592dfcca3b2e5446e67
ChHungerbuehler <christian.hungerbuehler@stud.unibas.ch>
2024-04-24 19:33:16 +0200

    adjusting spaces

--- shell.c
+++ shell.c
@@ -21,18 +21,18 @@ void space_parser() {
 
 /* combines pipe_detector() and space_parser() */
 char ** split(char *in_str, char* delim) {
-		// count how often the delimiter delim appears in the string in_str
-		char *tmp = in_str;
-		int counter = 0;
-		char **out_str;
+    // count how often the delimiter delim appears in the string in_str
+    char *tmp = in_str;
+    int counter = 0;
+    char **out_str;
 		
-		while (*tmp) {
-				if (*tmp == *delim) {
-						counter++;
-				}
-				tmp++;
-		}
-		// https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
+    while (*tmp) {
+        if (*tmp == *delim) {
+            counter++;
+        }
+        tmp++;
+    }
+    // https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
 		
 }
 
@@ -48,12 +48,12 @@ void cmd_finder() {
 
 //main funciton of customized shell
 int main () {
-		char user_input[MAX_INPUT_LENGTH];
- 		while(1) {
- 				printf("$ ");
- 				scanf("%s", user_input);
- 				// call parser
- 				printf("Input: %s\n", user_input);
- 		}  
+    char user_input[MAX_INPUT_LENGTH];
+    while(1) {
+        printf("$ ");
+        scanf("%s", user_input);
+        // call parser
+        printf("Input: %s\n", user_input);
+    }  
     return 0;
 }
