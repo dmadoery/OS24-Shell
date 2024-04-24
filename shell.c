906a2433c1c152428faa0f489f15de02601ddf79
dmadoery <daniel@madoery.com>
2024-04-24 11:59:31 +0200

    added shell main file with the code structure in it.

---
+++ shell.c
@@ -0,0 +1,34 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<sys/wait.h> 
+//#include<readline/readline.h> 
+//#include<readline/history.h> 
+
+//parser for "|"
+void pipe_detector() {
+    //TODO Implement
+}
+
+//parser for " "
+void space_parser() {
+    //TODO Implement
+}
+
+//executes piped and single commands via child processes with pipe communication if there are multiple cmds
+void exe_cmd() {
+    //TODO Implement
+}
+
+//switch case for all cmds
+void cmd_finder() {
+    //TODO Not sure if needed as own function
+}
+
+//main funciton of customized shell
+int main () {
+    
+    return 0;
+}
