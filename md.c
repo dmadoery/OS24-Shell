01930b7fdb9517f68e05e0b850e184cb778dc4e9
ChHungerbuehler <christian.hungerbuehler@stud.unibas.ch>
2024-05-10 00:01:28 +0200

    ensure correct format md dir_name

--- md.c
+++ md.c
@@ -17,13 +17,13 @@ int md(const char *dir_name) {
 /*
 receives input in the format
 	command = ./md = argv[0]
-	flag = NULL = argv[1]
+	flag = "-" = argv[1]
 	inpu1 = dir_name = argv[2]
-	input2 = NULL = argv[3]
+	input2 = "-" = argv[3]
 */
 int main(int argc, char **argv) {
-	if (argc != 4) {
-		printf("Syntax error - expected format: md NULL dir_name NULL");
+	if (argc != 4 || *argv[1] != '-' || *argv[2] == '-' || *argv[3] != '-') {
+		printf("Syntax error - expected format: md dir_name\n"); // note: user input format differs from input format receivde from shell
 		return -1;
 	}
 	int ret = md(argv[2]);
