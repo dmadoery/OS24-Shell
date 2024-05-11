56710f74a293bdabb45384d9c0b3681500ecc372
dmadoery <daniel@madoery.com>
2024-05-11 21:19:35 +0200

    pwd working

--- pwd.c
+++ pwd.c
@@ -10,25 +10,13 @@
 int pwd() {
 	char cwd[PATH_MAX];	// cwd = current working directory
 	if (getcwd(cwd, sizeof(cwd)) != NULL) {	// copies an absolute pathname of the current working directory to cwd
-		printf("%s\n", cwd);
+		printf("$Current directory: %s\n",cwd);
 	} else {
 		return errno;
 	}
 }
 
-/*
-receives input in the format
-	command = ./md = argv[0]
-	flag = "-" = argv[1]
-	inpu1 = dir_name = argv[2]
-	input2 = "-" = argv[3]
-*/
 int main(int argc, char **argv) {
-    printf("testing");
-	if (argv[0] != "pwd") {
-		printf("Syntax error - expected format: pwd\n");
-		return -1;
-	}
-	pwd();
+    pwd();
 	return 0;
 }
