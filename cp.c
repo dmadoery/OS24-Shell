8cff9cec28df58c8227beb788eece502a49aba32
ChHungerbuehler <christian.hungerbuehler@stud.unibas.ch>
2024-06-02 21:27:22 +0200

    updating cp.c

--- cp.c
+++ cp.c
@@ -1,5 +1,10 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#include "cmd.h" // PATH_MAX
 
 /*
 receives input in the format
@@ -13,9 +18,36 @@ int main(int argc, char **argv) {
 		printf("Syntax error - expected format: cp <from_file> <to_file>\n"); // note: user input format differs from input format receivde from shell
 		return -1;
 	}
+	// open shared memory
+	char *cwd;
+	int shmfd = shm_open(SHM_NAME, O_RDWR, S_IRUSR | S_IWUSR);
+	if (shmfd == -1) {
+		printf("[pwd2] shm_open failed\n");
+	}
+	struct dfshm *data = mmap(NULL, sizeof(struct dfshm), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+	if (data == NULL) {
+		printf("[pwd2] mmap failed\n");
+	}
+	cwd = data->current_working_dir;
+	
+	int m = strlen(cwd);
+	int n = strlen(argv[2]);
+	char from_path_file[m + n + 2];
+	
+	for (int i = 0; i < m; i++) {
+		from_path_file[i] = cwd[i];
+	}
+	from_path_file[m] = '/';
+	for (int i = 0; i < n; i++) {
+		from_path_file[m + 1 + i] = argv[2][i];
+	}
+	from_path_file[m + n + 1] = '\0';
+	
+	
+	// open files
 	FILE *from_file;
 	FILE *to_file;
-	from_file = fopen(argv[2], "r");
+	from_file = fopen(from_path_file, "r");
 	if (from_file == NULL) {
 		perror("mf: fopen() failed for from_file\n");	// printf() may be redirected
 		exit(1);
