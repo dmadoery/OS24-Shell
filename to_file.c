09f8d40d213d57e117930d115be2a2d6ca38fd9f
ChHungerbuehler <christian.hungerbuehler@stud.unibas.ch>
2024-06-05 15:43:08 +0200

    updating to_file.c

--- to_file.c
+++ to_file.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LINE 128
+
 /*
 receives input in the format
 	command = "./to_file" = argv[0]
@@ -10,17 +12,16 @@ receives input in the format
 */
 int main(int argc, char **argv) {
 	if (argc != 4 || *argv[1] != '-' || *argv[2] == '-' || *argv[3] != '-') {
-		printf("Syntax error - expected format: to_file <to_file>\n"); // note: user input format differs from input format receivde from shell
-		return -1;
+		perror("Syntax error - expected format: to_file <[to_path/]to_file>\n"); // note: user input format differs from input format receivde from shell
+		exit(1);
 	}
 	FILE *to_file;
 	to_file = fopen(argv[2], "w");
 	if (to_file == NULL) {
-		perror("mf: fopen() failed for to_file\n");	// printf() may be redirected
+		perror("ERROR[to_file main]: fopen() failed for to_file\n");	// printf() may be redirected
 		exit(1);
 	}
-	printf("(1)\n");
-	char buffer[128];
+	char buffer[MAX_LINE];
 	while(fgets(buffer, sizeof(buffer), stdin)) {
 		fprintf(to_file, "%s", buffer);
 	}
